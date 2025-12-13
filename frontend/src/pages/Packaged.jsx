ab2de892643b8a93180387fc18cab1aa68c67e3c
Angel0010 <131789727+Angel0010@users.noreply.github.com>
2025-12-13 22:24:40 +0700

    Update Packaged.jsx

--- frontend/src/pages/Packaged.jsx
+++ frontend/src/pages/Packaged.jsx
@@ -4,7 +4,7 @@ export default function Packaged() {
   return (
     <section className="mx-auto max-w-6xl px-4 pt-0 pb-8">
       {/* TITLE */}
-      <h1 className="text-2xl sm:text-3xl font-semibold text-slate-900">
+      <h1 className="text-2xl sm:text-3xl font-semibold text-slate-900 ">
         Packaged Products
       </h1>
       <p className="mt-1 text-sm text-slate-500">
